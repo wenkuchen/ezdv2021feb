18e2b0b29145520ee4f75d39fe1e5ca613e36aee
Wayne Chen <wenkuchen@gmail.com>
2021-02-26 12:49:39 +0800

    cleaning default react create app and mod nav.js

---
+++ src/components/nav/nav.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Nav () {
+  return (
+    <div className="nav-container">
+      <nav>
+          <h1>LogoHere</h1>
+      </nav>
+    </div>
+  );
+}
+
+export default Nav;
