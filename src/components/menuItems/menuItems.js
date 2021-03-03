1630f382652fa0dcfb9cffd4c6d6f0f026feeff5
Wayne Chen <wenkuchen@gmail.com>
2021-03-03 15:49:47 +0800

    using MenuItems.map() to list menu items in navMenu.js

---
+++ src/components/menuItems/menuItems.js
@@ -0,0 +1,7 @@
+const MenuItems = [
+        {title:"Home", url:"/"},
+        {title:"Services", url:"/services"},
+        {title:"About", url:"/about"}
+    ];
+
+export default MenuItems;
--- src/components/nav/navMenu.js
+++ src/components/nav/navMenu.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import MenuItems from '../menuItems/menuItems';
 
-function NavMenu () {
+const NavMenu = () => {
+    console.log(MenuItems);
   return (
     <ul className='nav-links'>
-        <li className='menu-item'>item1</li>
-        <li className='menu-item'>item2</li>
-        <li className='menu-item'>item3</li>
+        { 
+        MenuItems.map(
+            (item, index) => 
+                <li>{item.title}</li>
+             )
+        }
     </ul>
   );
 }
