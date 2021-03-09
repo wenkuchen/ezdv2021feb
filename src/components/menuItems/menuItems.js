41588a9167da8819cf4bda0163c6aa62e1a17c12
Wayne Chen <wenkuchen@gmail.com>
2021-03-09 16:21:13 +0800

    forget about iterated menu items from menu items array

--- src/components/menuItems/menuItems.js
+++ src/components/menuItems/menuItems.js
@@ -3,7 +3,7 @@ import Services from '../../pages/services';
 import About from '../../pages/about';
 
 const MenuItems = [
-        {title:"Home", url:"/",component:Home},
+        {title:"Home", url:"/", component:Home},
         {title:"Services", url:"/services",component:Services},
         {title:"About", url:"/about",component:About}
     ];
--- src/components/nav/navMenu.js
+++ src/components/nav/navMenu.js
@@ -2,6 +2,10 @@ import React from 'react';
 import {Link, BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import MenuItems from '../menuItems/menuItems';
 
+import Home from '../../pages/home';
+import Services from '../../pages/services';
+import About from '../../pages/about';
+
 const NavMenu = () => {
     console.log(MenuItems);
   return (
@@ -16,17 +20,22 @@ const NavMenu = () => {
              )
         }
     </ul>
+    
     <Switch>
       {
         MenuItems.map(
           (item,index) =>
-            <Route exact path={item.url} component={item.component}> </Route>
+            <Route exact path={item.url} component={item.component}></Route>
         )
-      }
-        <Route >No Match</Route>
+      } 
+        <Route exact path="/"><Home/></Route>
+        <Route path="/services"><Services/></Route>
+        <Route path="/about"><About/></Route>
+        <Route >No Match<Home/></Route>
     </Switch>
+    
     </Router>
-  )
+  ) // the Switch menu items iterated from MenuItems.js not working
 } 
 
 export default NavMenu;
